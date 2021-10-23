c0330b6c7314bb87196c844d1926d27be232a59e
Ethan <hi@ethanlipnik.com>
2021-10-23 13:54:25 -0400

    Initial Commit

---
+++ Acrylic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Acrylic
+//
+//  Created by Ethan Lipnik on 10/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

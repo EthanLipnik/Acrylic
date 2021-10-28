4bc2bf0e3f32dd6bef676ae5fca6f75204f693b8
Ethan Lipnik <hi@ethanlipnik.com>
2021-10-27 21:55:50 -0400

    Added dynamic grabber views

--- Acrylic/EditorViewController.swift
+++ Acrylic/EditorViewController.swift
@@ -29,24 +29,6 @@ class EditorViewController: UIViewController {
         return view
     }()
     
-    lazy var grabberView: UIView = {
-        let view = UIView()
-        
-        view.backgroundColor = UIColor.secondarySystemFill
-        view.layer.cornerRadius = 25
-        
-        view.layer.shadowOffset = CGSize(width: 0, height: 4)
-        view.layer.shadowRadius = 10
-        view.layer.shadowOpacity = 0.8
-        
-        let pointerInteraction = UIPointerInteraction(delegate: self)
-        view.addInteraction(pointerInteraction)
-        
-        view.translatesAutoresizingMaskIntoConstraints = false
-        
-        return view
-    }()
-    
     lazy var backgroundView: SBAVisualEffectView = {
         let view = SBAVisualEffectView(blurStyle: .systemUltraThinMaterial)
         
@@ -56,8 +38,13 @@ class EditorViewController: UIViewController {
         return view
     }()
     
-    lazy var grabberCenterXAnchor: NSLayoutConstraint = grabberView.centerXAnchor.constraint(equalTo: meshView.centerXAnchor)
-    lazy var grabberCenterYAnchor: NSLayoutConstraint = grabberView.centerYAnchor.constraint(equalTo: meshView.centerYAnchor)
+    lazy var grabbersView: GrabbersView = {
+        let view = GrabbersView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
     
     lazy var meshService: MeshService! = {
         (view.window?.windowScene?.delegate as? SceneDelegate)?.meshService
@@ -76,7 +63,7 @@ class EditorViewController: UIViewController {
         view.addSubview(backgroundView)
 #endif
         view.addSubview(meshView)
-        view.addSubview(grabberView)
+        meshView.addSubview(grabbersView)
         
         NSLayoutConstraint.activate([
             meshView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
@@ -85,16 +72,11 @@ class EditorViewController: UIViewController {
             meshView.widthAnchor.constraint(equalTo: meshView.heightAnchor),
             meshView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
             
-            grabberCenterXAnchor,
-            grabberCenterYAnchor,
-            grabberView.widthAnchor.constraint(equalToConstant: 50),
-            grabberView.heightAnchor.constraint(equalTo: grabberView.widthAnchor)
+            grabbersView.leadingAnchor.constraint(equalTo: meshView.leadingAnchor),
+            grabbersView.trailingAnchor.constraint(equalTo: meshView.trailingAnchor),
+            grabbersView.bottomAnchor.constraint(equalTo: meshView.bottomAnchor),
+            grabbersView.topAnchor.constraint(equalTo: meshView.topAnchor)
         ])
-        
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(updateGesture))
-        panGesture.allowedScrollTypesMask = [.all]
-        
-        grabberView.addGestureRecognizer(panGesture)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -104,6 +86,8 @@ class EditorViewController: UIViewController {
             .sink { [weak self] colors in
                 guard let self = self else { return }
                 self.meshView.create(colors, width: self.meshService.width, height: self.meshService.height, subdivisions: self.meshService.subdivsions)
+                
+                self.grabbersView.setPoints(colors, width: self.meshService.width, height: self.meshService.height)
             }
             .store(in: &cancellables)
         
@@ -136,46 +120,6 @@ class EditorViewController: UIViewController {
             ]
         }
     }
-    
-    @objc func updateGesture(_ recognizer: UIPanGestureRecognizer) {
-        let location = recognizer.location(in: meshView)
-        
-        let xLocation = location.x - (meshView.bounds.width / 2)
-        let minXLocation = max(xLocation, -meshView.bounds.width / 2)
-        let maxXLocation = min(minXLocation, meshView.bounds.width / 2)
-        grabberCenterXAnchor.constant = maxXLocation
-        
-        let yLocation = location.y - (meshView.bounds.height / 2)
-        let minYLocation = max(yLocation, -meshView.bounds.height / 2)
-        let maxYLocation = min(minYLocation, meshView.bounds.height / 2)
-        grabberCenterYAnchor.constant = maxYLocation
-        
-        UIView.animate(withDuration: 0.05, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction, .curveLinear]) {
-            self.view.layoutSubviews()
-        }
-        
-        let width = CGFloat(meshService.width) - 1
-        let height = CGFloat(meshService.height) - 1
-        
-        let x = min(width - 0.2, max(0.3, location.x / (meshView.bounds.width / width)))
-        let y = height - min(height - 0.5, max(0.35, location.y / (meshView.bounds.height / height)))
-        
-        if let sceneDelegate = view.window?.windowScene?.delegate as? SceneDelegate {
-            let meshService = sceneDelegate.meshService
-            
-            if let index = meshService.colors.firstIndex(where: { $0.point.x == 1 && $0.point.y == 1 }) {
-                meshService.colors[index].location = (Float(x), Float(y))
-            }
-        }
-    }
-}
-
-extension EditorViewController: UIPointerInteractionDelegate {
-#if !targetEnvironment(macCatalyst)
-    func pointerInteraction(_ interaction: UIPointerInteraction, styleFor region: UIPointerRegion) -> UIPointerStyle? {
-        return .hidden()
-    }
-#endif
 }
 
 struct EditorView: UIViewControllerRepresentable {
@@ -186,4 +130,125 @@ struct EditorView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: EditorViewController, context: Context) {
         
     }
+}
+
+class GrabbersView: UIView {
+    
+    private(set) lazy var grabberViews: [GrabberView] = []
+    private(set) var width: Int = 0
+    private(set) var height: Int = 0
+    
+    func setPoints(_ colors: [MeshNode.Color], width: Int, height: Int) {
+        self.width = width
+        self.height = height
+        
+        if subviews.isEmpty {
+            grabberViews = colors.map({ createGrabber($0) })
+            grabberViews.forEach(addSubview)
+        } else {
+            grabberViews.forEach { [bounds] grabber in
+                if let color = colors.first(where: { $0.point == grabber.node.point }) {
+                    grabber.updateLocation(color.location, meshSize: CGSize(width: width, height: height), size: CGSize(width: bounds.width, height: bounds.height))
+                }
+            }
+        }
+    }
+    
+    final func createGrabber(_ node: MeshNode.Color) -> GrabberView {
+        let view = GrabberView(node, meshSize: CGSize(width: width, height: height), parentSize: bounds.size)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        let pointerInteraction = UIPointerInteraction(delegate: self)
+        view.addInteraction(pointerInteraction)
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(updateGesture))
+        panGesture.allowedScrollTypesMask = [.all]
+        
+        view.addGestureRecognizer(panGesture)
+        
+        return view
+    }
+    
+    @objc func updateGesture(_ recognizer: UIPanGestureRecognizer) {
+        guard let grabberView = grabberViews.first(where: { $0.node == (recognizer.view as? GrabberView)?.node }) else { return }
+        let location = recognizer.location(in: self)
+        
+        print(grabberView.node)
+        
+//        let xLocation = location.x - (bounds.width / 2)
+//        let minXLocation = max(xLocation, -bounds.width / 2)
+//        let maxXLocation = min(minXLocation, bounds.width / 2)
+//        grabberViews[viewIndex].center.x = maxXLocation
+//
+//        let yLocation = location.y - (bounds.height / 2)
+//        let minYLocation = max(yLocation, -bounds.height / 2)
+//        let maxYLocation = min(minYLocation, bounds.height / 2)
+//        grabberViews[viewIndex].center.y = maxYLocation
+        
+//        UIView.animate(withDuration: 0.05, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction, .curveLinear]) { [weak self] in
+//            self?.layoutSubviews()
+//        }
+        
+        let width = CGFloat(width) - 1
+        let height = CGFloat(height) - 1
+        
+        let x = min(width, max(0, location.x / (bounds.width / width)))
+        let y = height - min(height, max(0, location.y / (bounds.height / height)))
+        
+        if let sceneDelegate = window?.windowScene?.delegate as? SceneDelegate {
+            let meshService = sceneDelegate.meshService
+            
+            if let index = meshService.colors.firstIndex(where: { $0.point == grabberView.node.point }) {
+                meshService.colors[index].location = (Float(x), Float(y))
+            }
+        }
+    }
+    
+    class GrabberView: UIView {
+        var node: MeshNode.Color
+        
+        init(_ node: MeshNode.Color, meshSize: CGSize, parentSize: CGSize? = nil) {
+            self.node = node
+            super.init(frame: .init(origin: .zero, size: .init(width: 50, height: 50)))
+            setup(meshSize: meshSize, parentSize: parentSize)
+        }
+        
+        override init(frame: CGRect) {
+            fatalError()
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        final func setup(meshSize: CGSize, parentSize: CGSize?) {
+            backgroundColor = UIColor.secondarySystemFill
+            layer.cornerRadius = bounds.width / 2
+            
+            layer.shadowOffset = CGSize(width: 0, height: 4)
+            layer.shadowRadius = 10
+            layer.shadowOpacity = 0.8
+            
+            if let size = parentSize {
+                updateLocation(node.location, meshSize: meshSize, size: size)
+            }
+        }
+        
+        final func updateLocation(_ location: (x: Float, y: Float), meshSize: CGSize, size: CGSize) {
+            let point = CGPoint(x: (size.width / (meshSize.width - 1)) * CGFloat(location.x),
+                                y: size.height - ((size.height / (meshSize.height - 1)) * CGFloat(location.y)))
+            
+            UIView.animate(withDuration: 0.05, delay: 0, options: [.beginFromCurrentState, .allowUserInteraction, .curveLinear]) { [weak self] in
+                self?.center = point
+            }
+        }
+    }
+}
+
+extension GrabbersView: UIPointerInteractionDelegate {
+#if !targetEnvironment(macCatalyst)
+    func pointerInteraction(_ interaction: UIPointerInteraction, styleFor region: UIPointerRegion) -> UIPointerStyle? {
+        return .hidden()
+    }
+#endif
 }

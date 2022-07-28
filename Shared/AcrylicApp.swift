b17eb4ba5fd004f81945d22d0ce967df7adc7836
EthanLipnik <hi@ethanlipnik.com>
2022-07-28 15:30:21 -0400

    start a new project

---
+++ Shared/AcrylicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcrylicApp.swift
+//  Shared
+//
+//  Created by Ethan Lipnik on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct AcrylicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

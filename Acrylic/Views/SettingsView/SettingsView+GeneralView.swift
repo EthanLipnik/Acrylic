6110948c2ee30396beca40d7ae2b9df3d477ae13
Ethan Lipnik <hi@ethanlipnik.com>
2022-09-18 17:31:04 -0400

    add launch at login for macOS 13

--- Acrylic/Views/SettingsView/SettingsView+GeneralView.swift
+++ Acrylic/Views/SettingsView/SettingsView+GeneralView.swift
@@ -6,16 +6,37 @@
 //
 
 import SwiftUI
+import ServiceManagement
 
 extension SettingsView {
     struct GeneralView: View {
         @AppStorage("colorSpace") var colorSpace: ColorSpace = .sRGB
+        @AppStorage("launchAtLogin") var launchAtLogin: Bool = false
 
         var body: some View {
             SectionView {
-                Toggle(isOn: .constant(false)) {
-                    Text("Launch Acrylic on system startup")
-                }.disabled(true)
+                if #available(macOS 13.0, *) {
+                    Toggle(isOn: $launchAtLogin) {
+                        Text("Launch Acrylic on system startup")
+                    }
+                    .onChange(of: launchAtLogin) { newValue in
+                        let bundleId = "com.ethanlipnik.Acrylic.AutoLauncher"
+                        
+                        do {
+                            let item = SMAppService.mainApp
+                            if newValue {
+                                try item.register()
+                            } else {
+                                try item.unregister()
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                    .onAppear {
+                        launchAtLogin = SMAppService.mainApp.status == .enabled
+                    }
+                }
 
                 Picker("Color Space", selection: $colorSpace) {
                     ForEach(ColorSpace.allCases, id: \.rawValue) { colorSpace in
